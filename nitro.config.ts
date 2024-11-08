57973a6c42db5b2ed56839dfb1e995c0a35897c8
Mick Lawitzke <info@lawitzke.de>
2024-11-08 14:54:55 +0100

    first commit

---
+++ nitro.config.ts
@@ -0,0 +1,5 @@
+//https://nitro.unjs.io/config
+export default defineNitroConfig({
+  ignore: ['/dev-only'],
+  compatibilityDate: '2024-11-08'
+});
---
+++ routes/dev-only/some-route.ts
@@ -0,0 +1,3 @@
+export default defineEventHandler(async (event) => {
+    return 'This should not be accessible';
+});
